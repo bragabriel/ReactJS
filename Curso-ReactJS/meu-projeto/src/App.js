590a87f2a78e5fcf8fc3ebc8baa405557f6c10b0
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-14 11:15:43 -0300

    feat: JSX

--- Curso-ReactJS/meu-projeto/src/App.js
+++ Curso-ReactJS/meu-projeto/src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Gabriel"
+
+  const newName = name.toUpperCase()
+
+  function sum(a,b){
+    return a+b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
       <h1>Olá React!</h1>
       <p>Meu primeiro APP em React! =D</p>
+      <br></br>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}; Soma: {2 + 2}; Func: {sum(5, 5)}</p>
+      <img src={url} alt=""></img>
     </div>
   );
 }
