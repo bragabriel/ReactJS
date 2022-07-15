60174e257edfffef7e5e85d4cbc0055f2dc2605e
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-15 15:22:59 -0300

    feat: conditional render

--- Curso-ReactJS/meu-projeto2/src/App.js
+++ Curso-ReactJS/meu-projeto2/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Condicional from './components/Condicional';
 import Evento from './components/Evento'
 import Form from './components/Form'
 
@@ -9,6 +10,11 @@ function App() {
      <h1>Testando Eventos</h1>
      <Evento numero="1"/>
      <Form/>
+    
+     <br /><br />
+    
+     <h1>Renderização Condicional:</h1>
+     <Condicional/>
     </div>
   );
 }
