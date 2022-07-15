d57f263b2347ecff6ab2de1750ff6dd579db3a81
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-15 11:16:36 -0300

    feat: props advanced

--- Curso-ReactJS/meu-projeto/src/components/Item.js
+++ Curso-ReactJS/meu-projeto/src/components/Item.js
@@ -1,10 +1,24 @@
-function Item(props){
+import PropTypes from 'prop-types'
+
+function Item({marca, ano_lancamento}){
     return(
         <>
-            <li>{props.marca}</li>
-            <p>Descrição: asdasd</p>
+            {/* <li>{props.marca}</li> */}
+            <li>{marca} - {ano_lancamento}</li>
+            {/* <p>Descrição: asdasd</p> */}
         </>
     )
+}
+
+/* Propriedade do componente <-> Validação a nível de programação: vai exibir na tela, mas dá erro no console*/
+Item.propTypes = {
+    marca: PropTypes.string.isRequired, /* Dizendo que a marca é uma String e é Obrigatória */
+    ano_lancamento: PropTypes.number,
+}
+
+Item.defaultProps = {
+    marca: 'Faltou a marca',
+    ano_lancamento: 'X',
 }
 
 export default Item
--- Curso-ReactJS/meu-projeto/src/components/List.js
+++ Curso-ReactJS/meu-projeto/src/components/List.js
@@ -5,9 +5,11 @@ function List(){
         <> {/* <-- Fragment */} 
             <h1>Minha Lista - Sem elemento pai, utilizando Fragment</h1>
             <ul>
-                <Item marca="Ferrari"/>
-                <Item marca="Lamborghini"/>
+                <Item marca="Ferrari" ano_lancamento={1985}/>
+                <Item marca="Lamborghini" ano_lancamento={1964}/>
                 <Item marca="Renault"/>
+                <Item marca="Chevrolet" ano_lancamento="1999"/>
+                <Item/>
             </ul>
         </>
     )
